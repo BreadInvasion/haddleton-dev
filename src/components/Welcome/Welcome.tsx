20a7389a4d9ac12950e400946755cb044131ea76
Mark <haddlm@rpi.edu>
2023-11-07 15:50:53 -0500

    Initial yarn lock regen and fiddling

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -4,12 +4,13 @@ import classes from './Welcome.module.css';
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+      <Title className={classes.title} ta="center">
+        Mark{' '}
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'darkred' }}>
+          Haddleton
         </Text>
       </Title>
+      <Title className={classes.subtitle} ta="center">Project Portfolio</Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         This starter Vite project includes a minimal setup, if you want to learn more on Mantine +
         Vite integration follow{' '}
