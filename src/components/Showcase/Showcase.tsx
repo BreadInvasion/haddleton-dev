fc39214789a1096c9b5a827d37b0a79925dcc218
Mark Haddleton <haddlm@rpi.edu>
2023-11-07 23:42:23 -0500

    Reword

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -7,7 +7,7 @@ export function Showcase() {
     <>
       <Group grow mt="40">
         <Button disabled color={`primaryShades.${computedColorScheme ? '1' : '10'}`} variant="light">Software</Button>
-        <Button color={`primaryShades.${computedColorScheme ? '1' : '10'}`} variant="light">Game Dev</Button>
+        <Button color={`primaryShades.${computedColorScheme ? '1' : '10'}`} variant="light">Game Development</Button>
         <Button color={`primaryShades.${computedColorScheme ? '1' : '10'}`} variant="light">Engineering</Button>
       </Group>
     </>
